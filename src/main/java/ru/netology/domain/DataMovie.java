697441ca0d138352c8c519d1a12d93a92e642d6c
Alisa68 <lebedeva19682@yandex.ru>
2022-01-23 12:12:08 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/DataMovie.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+
+
+public class DataMovie {
+    private int id;
+    private String movieName;
+    private String movieGenre;
+    private boolean premiere;
+
+}
---
+++ src/main/java/ru/netology/manager/ManagerMovie.java
@@ -0,0 +1,66 @@
+package ru.netology.manager;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import ru.netology.domain.DataMovie;
+
+@Data
+@NoArgsConstructor
+
+public class ManagerMovie {
+    DataMovie[] items = {new DataMovie(1, "Soul", "cartoon", false),
+            new DataMovie(2, "Call me", "action", false),
+            new DataMovie(3, "Gentlemen", "action", false),
+            new DataMovie(4, "Invisible", "Thriller", false),
+            new DataMovie(5, "Troll's tour", "cartoon", true),
+            new DataMovie(6, "I BelieveInLove", "romance", true),
+            new DataMovie(7, "Pinocchio", "children", true)};
+
+    DataMovie film = new DataMovie(8, "House of Cards", "serials", true);
+
+    private int moviesQuantity = 5;
+
+
+    //Метод для добавления фильмов
+    public DataMovie[] addMovie(DataMovie film) {
+        int lenght = items.length + 1;
+        DataMovie[] tmp = new DataMovie[lenght];
+        // for (int i = 0; i < items.length; i++) {
+        // tmp[i]=items[i];
+        //}
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = this.film;
+        items = tmp;
+
+
+        return items;
+    }
+
+    //Метод получения всего списка элементов
+    public DataMovie[] getAllMovies() {
+        DataMovie[] tmp = new DataMovie[items.length];
+        // перебираем массив в прямом порядке, кладем в обратном
+        for (int i = 0; i < tmp.length; i++) {
+            int index = items.length - i - 1;
+            tmp[i] = items[index];
+        }
+        return tmp;
+    }
+
+
+    //Метод, которотый выводит нужное количество фильмов
+    public DataMovie[] getTheRightAmountMovies() {
+        int count = this.moviesQuantity;
+        if (count > items.length)
+            count = items.length;
+        DataMovie[] answer = new DataMovie[count];
+        for (int i = 0; i < answer.length; i++) {
+            int index = items.length - i - 1;
+            answer[i] = items[index];
+        }
+        return answer;
+    }
+}
